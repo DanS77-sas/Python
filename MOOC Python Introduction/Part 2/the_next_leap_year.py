3f950dc302675d0617d74e32ecfe21911519ec96
DanS77-sas <Sas.Daniel@outlook.com>
2025-06-13 09:53:57 +0100

    Update the_next_leap_year.py

--- MOOC Python Introduction/Part 2/the_next_leap_year.py
+++ MOOC Python Introduction/Part 2/the_next_leap_year.py
@@ -3,7 +3,7 @@ year = int(input("Year: "))
 next_year = year + 1
 
 while True:
-    #Checks if year is dvisible by 400 or 4 but NOT by 100.
+    #Checks if year is divisible by 400 or 4 but NOT by 100.
     if next_year % 400 == 0 or (next_year % 4 == 0 and next_year % 100 != 0):
         print(f"The next leap year after {year} is {next_year}")
         break
